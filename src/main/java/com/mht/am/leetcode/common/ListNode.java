9c2e79a97dd7c96a5a796721981c84c17a0db479
aoming <921742077@qq.com>
2020-09-21 18:19:33 +0800

    leet 2020.9.21

---
+++ src/main/java/com/mht/am/leetcode/common/ListNode.java
@@ -0,0 +1,13 @@
+package com.mht.am.leetcode.common;
+
+/**
+ * @classname: ListNode
+ * @description:
+ * @author: am
+ * @create: 2020-09-21 17:39
+ **/
+public class ListNode {
+    public int val;
+    public ListNode next;
+    ListNode(int x) { val = x; }
+}
