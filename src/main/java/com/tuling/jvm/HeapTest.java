906a4cb8d902e1ccf61db8eb7c967a451767b45f
aomii <921742077@qq.com>
2022-03-31 23:22:07 +0800

    jvm 1.2 内存模型

---
+++ src/main/java/com/tuling/jvm/HeapTest.java
@@ -0,0 +1,24 @@
+package com.tuling.jvm;
+
+import java.util.ArrayList;
+
+/**
+ * TODO
+ *
+ * @author ao921
+ * @version 1.0
+ * @date 2022/3/30 13:24
+ */
+public class HeapTest {
+    byte [] a=new byte[1024*1024];
+
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<HeapTest> heapTests = new ArrayList<>();
+        while (true){
+            heapTests.add(new HeapTest());
+            Thread.sleep(10);
+        }
+    }
+
+}
+
